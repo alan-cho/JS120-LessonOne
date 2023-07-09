b1920740b755ebb74d938be75a71daeaa5f4d4b2
Alan Cho <alancho397@gmail.com>
2023-07-09 15:49:19 -0500

    Added win conditions

--- rps.js
+++ rps.js
@@ -34,24 +34,6 @@ function createPlayer(playerType) {
   };
 }
 
-function createMove() {
-  return {
-    // possible state: type of move (paper, rock, scissors)
-  };
-}
-
-function createRule() {
-  return {
-    // possible state? not clear whether Rules need state
-  };
-}
-
-// Since we don't yet know where to put `compare`, let's define
-// it as an ordinary function.
-let compare = function (move1, move2) {
-  // not yet implemented
-};
-
 const RPSGame = {
   human: createPlayer("human"),
   computer: createPlayer("computer"),
@@ -64,11 +46,45 @@ const RPSGame = {
     console.log("Thanks for playing Rock, Paper, Scissors. Goodbye!");
   },
 
+  displayWinner() {
+    let humanMove = this.human.move;
+    let computerMove = this.computer.move;
+    console.log(
+      `You chose ${humanMove}, and the computer chose ${computerMove}.`
+    );
+
+    if (
+      (humanMove === "rock" && computerMove === "scissors") ||
+      (humanMove === "paper" && computerMove === "rock") ||
+      (humanMove === "scissors" && computerMove === "paper")
+    ) {
+      console.log("Player wins!");
+    } else if (
+      (humanMove === "rock" && computerMove === "paper") ||
+      (humanMove === "paper" && computerMove === "scissors") ||
+      (humanMove === "scissors" && computerMove === "rock")
+    ) {
+      console.log("Computer wins!");
+    } else {
+      console.log("It's a draw!");
+    }
+  },
+
+  playAgain() {
+    let answer = readline.question("Would you like to play again (y or n)?");
+    return answer.toLowerCase()[0] === "y";
+  },
+
   play() {
     this.displayWelcomeMessage();
-    this.human.choose();
-    this.computer.choose();
-    displayWinner();
+    while (true) {
+      this.human.choose();
+      this.computer.choose();
+      this.displayWinner();
+      if (!this.playAgain()) {
+        break;
+      }
+    }
     this.displayGoodbyeMessage();
   },
 };
